b30acb3e403adb53ccfda1bd448cfb448100d865
ardelvito <ardelvito8738@gmail.com>
2024-02-02 08:05:49 +0700

    005

---
+++ 005-Convert boolean values to strings Yes or No.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+// 8 kyu
+// Convert boolean values to strings 'Yes' or 'No'.
+
+function boolToWord( bool ){
+    return (bool == true) ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(false));
+
+
